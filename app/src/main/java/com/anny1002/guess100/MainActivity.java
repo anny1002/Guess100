2be713e258399c0879884834f372fd26a667e818
anny1002 <anny1002wade@gmail.com>
2017-11-14 10:00:05 +0800

    findViews

--- app/src/main/java/com/anny1002/guess100/MainActivity.java
+++ app/src/main/java/com/anny1002/guess100/MainActivity.java
@@ -2,6 +2,8 @@ package com.anny1002.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void findViews(View view){
+        EditText edNumber = (EditText) findViewById(R.id.number);
+        int number=Integer.parseInt(edNumber.getText().toString());
+
     }
 }
