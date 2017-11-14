76d0e0931f08d0a80eeff4fd0fa01dcd06f627c6
anny1002 <anny1002wade@gmail.com>
2017-11-14 10:47:35 +0800

    Guess

--- app/src/main/java/com/anny1002/guess100/MainActivity.java
+++ app/src/main/java/com/anny1002/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anny1002.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int secret =(int)(Math.random()*100+1);
-
-
-
+    int min=1,max=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +19,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findViews(View view){
-        EditText number;
-        TextView secret;
+        EditText edNumber;
+        TextView tvsecret;
         TextView info;
         Button send;
-        EditText edNumber = (EditText) findViewById(R.id.number);
+        tvsecret= (TextView) findViewById(R.id.secret);
+        edNumber = (EditText) findViewById(R.id.number);
         int n=Integer.parseInt(edNumber.getText().toString());
-
+        String s = String.valueOf(secret);
+        tvsecret.setText(s);
+        if(secret==n){
+            new AlertDialog.Builder(this)
+                    .setMessage("答對了")
+                    .setPositiveButton("ok",null)
+                    .show();
+        }
+        if (secret>n){
+            min=n;
+            new AlertDialog.Builder(this)
+                    .setMessage("大一點,請輸入"+min+"~"+max)
+                    .setPositiveButton("ok",null)
+                    .show();
+        }
+        if(secret<n){
+            max=n;
+            new AlertDialog.Builder(this)
+                    .setMessage("小一點,請輸入"+min+"~"+max)
+                    .setPositiveButton("ok",null)
+                    .show();
+        }
 
     }
+
 }
